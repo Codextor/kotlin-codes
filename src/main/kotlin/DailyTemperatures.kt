8f78124ca3b5b28b6df596f63dc4ca0353f54dd9
Sandeep Pandey <fromsandeeppandey@gmail.com>
2024-01-31 14:33:11 +0530

    Add daily temperatures

---
+++ src/main/kotlin/DailyTemperatures.kt
@@ -0,0 +1,40 @@
+/**
+ * Given an array of integers temperatures represents the daily temperatures,
+ * return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature.
+ * If there is no future day for which this is possible, keep answer[i] == 0 instead.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: temperatures = [73,74,75,71,69,72,76,73]
+ * Output: [1,1,4,2,1,1,0,0]
+ * Example 2:
+ *
+ * Input: temperatures = [30,40,50,60]
+ * Output: [1,1,1,0]
+ * Example 3:
+ *
+ * Input: temperatures = [30,60,90]
+ * Output: [1,1,0]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= temperatures.length <= 10^5
+ * 30 <= temperatures[i] <= 100
+ * @see <a href="https://leetcode.com/problems/daily-temperatures/">LeetCode</a>
+ */
+fun dailyTemperatures(temperatures: IntArray): IntArray {
+    val stack = ArrayDeque<Pair<Int, Int>>()
+    val answer = IntArray(temperatures.size)
+
+    for (index in (0 until temperatures.size).reversed()) {
+        while (stack.isNotEmpty() && temperatures[index] >= stack.last().first) {
+            stack.removeLast()
+        }
+        answer[index] = if (stack.isEmpty()) 0 else stack.last().second - index
+        stack.add(Pair(temperatures[index], index))
+    }
+    return answer
+}
