b559bdc56adfa24292b53e9ed4626e23e388ad6e
Sandeep Pandey <fromsandeeppandey@gmail.com>
2024-03-07 23:59:59 +0530

    Add pass the pillow

---
+++ src/test/kotlin/PassThePillowTest.kt
@@ -0,0 +1,19 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class PassThePillowTest {
+
+    @Test
+    fun testIsIsomorphic() {
+        val testCases = listOf(
+            Triple(4, 5, 2),
+            Triple(3, 2, 3)
+        )
+
+        testCases.forEach { testCase ->
+            val (n, time, expected) = testCase
+            val result = passThePillow(n, time)
+            assertEquals(expected, result, "Expected $expected but got $result for input s=$n, t=$time")
+        }
+    }
+}
